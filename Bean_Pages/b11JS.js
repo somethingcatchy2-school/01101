aedd78581b0cd7f8e2499844130d398924e4fbb0
Wise-Guy <153032279+somethingcatchy2-school@users.noreply.github.com>
2024-11-13 13:46:01 -0700

    beas

---
+++ Bean_Pages/b11JS.js
@@ -0,0 +1,22 @@
+let howpasta = 0;
+let givepasta = 1;
+function button() {
+  alert("PLEASE MEREGE INTO MAIN BRANCH OF REPO");
+  print("cheese");
+  console.log("I am in the console!");
+  document.getElementById("Vscode_isn't_the_best_editor!").style.color = "#FFE4C4";
+  document.getElementById("yes").textContent = "This message has been redacted by the user.";
+}
+function pasta() {
+  console.log("pasta?");
+  howpasta = givepasta + howpasta;
+  document.getElementById("howpasta").textContent = howpasta;
+
+}
+
+function givepastas() {
+  console.log("pasta?");
+  givepasta = givepasta + 1;
+  document.getElementById("givepasta").textContent = givepasta;
+
+}
---
+++ Bean_Pages/beans.js
@@ -0,0 +1,10 @@
+var pasta = 3;
+var cost = 1;
+var pot_quality = 0;
+
+
+
+function button() {
+  alert("PLEASE MEREGE INTO MAIN BRANCH OF REPO")
+}
+
